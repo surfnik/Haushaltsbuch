19cc27c98f986519869ba90ed04e5e9707c7c42e
Klaus Schönhofen <surfnik@gmail.com>
2022-07-12 13:35:03 +0200

    Klassen Transaction + Profile erstellt

---
+++ Haushaltsbuch/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haushaltsbuch
+{
+    internal class Profile
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public List<Transaction> Transactions { get; private set; }
+
+        public Profile(string name, decimal balance)
+        {
+            Name = name;
+            Balance = balance;
+            Transactions = new List<Transaction>();
+        }
+    }
+}
---
+++ Haushaltsbuch/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haushaltsbuch
+{
+    internal class Transaction
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public Transaction(string name, decimal amount, DateTime date)
+        {
+            Name = name;
+            Amount = amount;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " | Datum: " + Date.ToShortDateString() + " | Betrag: " + Amount;
+        }
+    }
+}
